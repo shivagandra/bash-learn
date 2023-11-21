f1e5c1544e8dd3281ccebf57a04f686b20717c59
Shiva Krishna Gandra <shivagandra9664@gmail.com>
2023-11-21 07:31:46 +0000

    Test

--- new_commit.sh
+++ new_commit.sh
@@ -6,8 +6,6 @@ git fetch
 # Get the latest commit hash on the remote branch
 old_commit_hash=$(git rev-parse origin/HEAD~1)
 
-# Fetch the latest changes from the remote repository
-git fetch
 
 # Get the latest commit hash on the remote branch
 new_commit_hash=$(git rev-parse origin/HEAD)

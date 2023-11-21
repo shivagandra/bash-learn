05ad7f9cfc145869e41cac0f6a98e93107a01434
Shiva Krishna Gandra <shivagandra9664@gmail.com>
2023-11-21 07:29:52 +0000

    Test

--- new_commit.sh
+++ new_commit.sh
@@ -6,19 +6,13 @@ git fetch
 # Get the latest commit hash on the remote branch
 old_commit_hash=$(git rev-parse origin/HEAD)
 
-while true; do
-    # Fetch the latest changes from the remote repository
-    git fetch
+# Fetch the latest changes from the remote repository
+git fetch
 
-    # Get the latest commit hash on the remote branch
-    new_commit_hash=$(git rev-parse origin/HEAD)
+# Get the latest commit hash on the remote branch
+new_commit_hash=$(git rev-parse origin/HEAD)
 
-    if [ "$old_commit_hash" != "$new_commit_hash" ]; then
-        echo "New commit detected. Pulling changes..."
-        git pull
-        old_commit_hash=$new_commit_hash
-    fi
-
-    # Wait for a while before the next check
-    sleep 60
-done
+if [ "$old_commit_hash" != "$new_commit_hash" ]; then
+    echo "New commit detected. Pulling changes..."
+    git pull
+fi

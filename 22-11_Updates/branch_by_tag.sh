4c5e3f7440f1c3978fc346e6ccf9af7bc8533e90
Shiva Krishna Gandra <77003885+shivagandra@users.noreply.github.com>
2023-11-22 04:21:07 +0000

    Adding Updates

---
+++ 22-11_Updates/branch_by_tag.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Get the latest tag in the repository
+latest_tag=$(git describe --tags --abbrev=0)
+
+# Create a new branch based on the latest tag
+new_branch="branch_${latest_tag}"
+git checkout -b "$new_branch"
+
+# Push the new branch to the remote repository
+git push origin "$new_branch"
+
+echo "New branch $new_branch created based on tag $latest_tag"
